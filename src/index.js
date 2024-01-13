c6a6a900ba5fe797eaa8853271fe905f352e6f32
BaronDeFitenbah <119754277+BaronDeFitenbah@users.noreply.github.com>
2024-01-13 18:19:57 +0500

    gigi

--- src/index.js
+++ src/index.js
@@ -10,7 +10,25 @@ document.addEventListener('DOMContentLoaded', () => {
   i18next.init({
     lng: 'ru',
     debug: true,
-    resources,
+    translation: {
+      feedback: {
+        success: 'RSS успешно загружен',
+        invalidUrl: 'Ссылка должна быть валидным URL',
+        invalidRequired: 'Не должно быть пустым',
+        invalidNotOneOf: 'RSS уже существует',
+        invalidUnknown: 'Возникла неизвестная ошибка. Попробуйте еще раз',
+        invalidRSS: 'Ресурс не содержит валидный RSS',
+        invalidNetwork: 'Ошибка сети',
+        loading: 'Идет загрузка...',
+      },
+      modal: {
+        read: 'Читать полностью',
+        close: 'Закрыть',
+      },
+      posts: 'Посты',
+      feeds: 'Фиды',
+      postButtonRead: 'Просмотр',
+    },
   })
     .then(() => console.log(i18n.t('feedback.invalidUrl')))
     .catch((e) => e.message);
