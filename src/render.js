fd2eb53bfc995855a753866f58de89782af0d71f
BaronDeFitenbah <119754277+BaronDeFitenbah@users.noreply.github.com>
2024-01-24 21:31:39 +0500

    Исправдения 2

--- src/render.js
+++ src/render.js
@@ -4,7 +4,7 @@ const renderFormError = (state, elements, i18n) => {
     input.classList.add('is-invalid');
     feedback.classList.remove('text-success');
     feedback.classList.add('text-danger');
-    errors = {
+    const errors = {
       'url':'errors.invalidUrl',
       'required':'errors.invalidRequired',
       'notOneOf':'errors.invalidNotOneOf',
